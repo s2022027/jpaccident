1327e4f56a34ad7e30fa71554902d69221effee3
Tsuyoshi Hine <72128878+s2022027@users.noreply.github.com>
2022-06-06 07:18:30 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="jpaccident",
-    version="0.0.2",
+    version="0.0.1",
     author="Tsuyoshi Hine",
     author_email="tsuyoshi.h0920@gmail.com",
     description='Package for visualization of aggregate data on fatalities in traffic accidents',
