6f2380b95dd65d613a490ab1aced683a614346f7
Tsuyoshi Hine <72128878+s2022027@users.noreply.github.com>
2022-06-06 08:06:22 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,5 +1,16 @@
-import setuptools
-import os
+#import setuptools
+#import os
+from setuptools import setup, find_packages
+from codes import open
+from os import path
+
+def _requires_from_file(filename:str):
+    return open(filename, encoding='utf-8').read().splitlines()
+
+def _read_readme():
+    return open(path.join(here, 'README.md'), encoding='utf-8').read().replace('\r','')
+
+here = path.abspath(path.dirname(__file__))
 
 #from pathlib import Path
 #this_directory = Path(__file__).parent
@@ -7,9 +18,9 @@ import os
 
 
 #with open("README.md", "r", encoding="utf-8") as fh:
-    #long_description = fh.read()
+ #   long_description = fh.read()
 
-setuptools.setup(
+setup(
     name="jpaccident",
     version="0.0.1",
     author="Tsuyoshi Hine",
@@ -17,8 +28,10 @@ setuptools.setup(
     description='Package for visualization of aggregate data on fatalities in traffic accidents',
     #long_description=long_description,
     #long_description_content_type="text/markdown",
+    long_description=_read_readme(),
     
-    long_description=open(path.join(here, 'README.md'), encoding='utf-8').read().replace("\r", "")
+    
+    #long_description=open(path.join(here, 'README.md'), encoding='utf-8').read().replace("\r", "")
     long_description_content_type="text/markdown",
     
     url="https://github.com/s2022027/jpaccident",
@@ -28,15 +41,16 @@ setuptools.setup(
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
+        #"Operating System :: OS Independent",
     ],
-    package_dir={"": "src"},
-    py_modules=['jpaccident'],
-    packages=setuptools.find_packages(where="src"),
-    python_requires=">=3.7",
+    #package_dir={"": "src"},
+    #py_modules=['jpaccident'],
+    #packages=setuptools.find_packages(where="src"),
+    #python_requires=">=3.7",
     entry_points = {
         'console_scripts': [
             'jpaccident = jpaccident:main'
         ]
     },
+    include_package_data=True,
 )
