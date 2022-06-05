20aae8f3f6d40f959309338d19147d6545210b13
Tsuyoshi Hine <72128878+s2022027@users.noreply.github.com>
2022-06-06 07:29:06 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ setuptools.setup(
     },
     classifiers=[
         "Programming Language :: Python :: 3",
-        #"License :: OSI Approved :: MIT License",
+        "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
     package_dir={"": "src"},
