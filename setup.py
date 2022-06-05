41aebad4476c99d7b7a1e6a151ce7901472b267a
Tsuyoshi Hine <72128878+s2022027@users.noreply.github.com>
2022-06-06 07:09:44 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,5 +1,10 @@
 import setuptools
 
+from pathlib import Path
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
+
 with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
